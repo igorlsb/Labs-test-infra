a664d4667d878645703b3f58527a091d22c88772
igor <igor.lsb.pro@gmail.com>
2025-02-04 17:22:59 -0300

    v9.2

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "🔍 Verificando se os certificados SSL já existem..."
+
+# Espera até que o Certbot gere os certificados
+while [ ! -f /etc/letsencrypt/live/frontend.dominiotest1.com/fullchain.pem ]; do
+    echo "⏳ Aguardando certificados SSL... (verificando a cada 5s)"
+    sleep 5
+done
+
+echo "✅ Certificado encontrado! Iniciando NGINX..."
+nginx -g "daemon off;"
